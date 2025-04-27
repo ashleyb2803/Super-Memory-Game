dd79430b6c4c2e3d01b66aaaf694c2b082a431c0
ashleyb2803 <ashleycarter2803@gmail.com>
2025-04-27 17:48:24 -0500

    add funtion render

--- Super-Memory/js/main.js
+++ Super-Memory/js/main.js
@@ -1,43 +1,37 @@
 /*----- constants -----*/
-    const CARDS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
-    const numberOfCards = CARDS.length * 2;
-
-  /*----- state variables -----*/
-    let shuffledCards = [];
-    let selectedCards = [];
-    let matchedCards = [];
-
-
-
-
-  /*----- cached elements  -----*/
-
-    const gameBoard = document.getElementById('game-board');
-
-
-
+// Each of the "card" objects will be copied twice,
+// then shuffled and used for the board's cards
+const SOURCE_CARDS = [
+    {img: 'https://i.imgur.com/ZXPKaiN.jpg', matched: false},
+    {img: 'https://i.imgur.com/XMEsZBX.jpg', matched: false},
+    {img: 'https://i.imgur.com/6jX1bMT.jpg', matched: false},
+    {img: 'https://i.imgur.com/yKdqsBv.jpg', matched: false},
+    {img: 'https://i.imgur.com/1BV3HLr.jpg', matched: false},
+    {img: 'https://i.imgur.com/QYmN6Hp.jpg', matched: false},
+    {img: 'https://i.imgur.com/D5pWE05.jpg', matched: false},
+    {img: 'https://i.imgur.com/Ss4Xo3x.jpg', matched: false}
+  ];
+  const CARD_BACK = 'https://i.imgur.com/WoEmI2M.jpg'; 
+  
+  /*----- app's state (variables) -----*/
+  let cards; // Array of 16 shuffled card objects
+  let firstCard; // The first card selected by the user
+  
+  /*----- cached element references -----*/
+  
+  
   /*----- event listeners -----*/
-
-  gameBoard.addEventListener('click', handleCardClick);
-
-
-
-
+  
+  
   /*----- functions -----*/
+  init();
+
+    // Initialize the game state and render the initial board
+  function init() {
     
-  // This intializes the game
-    function init() {
-    shuffledCards = shuffle(CARDS.concat(CARDS));
-    selectedCards = [];
-    matchedCards = [];
-    render();
-}
-
-// this shuffles the cards
-    function shuffle(array) {
-    return array.sort(() => Math.random() - 0.5);
-}
-
+  }
+  
+  // Initialize all state, then call render()
     
  
 
